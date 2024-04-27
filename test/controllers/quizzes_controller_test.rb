da77464e2e2066f0cc511a1c9bc08ad835de98d4
Kaede <happymaple428@gmail.com>
2024-04-28 02:32:01 +0900

    ヘッダーとプレイ・結果画面を追加

---
+++ test/controllers/quizzes_controller_test.rb
@@ -0,0 +1,8 @@
+require "test_helper"
+
+class QuizzesControllerTest < ActionDispatch::IntegrationTest
+  test "should get play" do
+    get quizzes_play_url
+    assert_response :success
+  end
+end
