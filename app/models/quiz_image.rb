7af960167208c2b95b26ebb09ecc074346b94213
Kaede <happymaple428@gmail.com>
2024-04-29 16:34:09 +0900

    クイズ用データの追加

--- app/models/quiz_image.rb
+++ app/models/quiz_image.rb
@@ -1,3 +1,3 @@
 class QuizImage < ApplicationRecord
-    validates :image, presence: true, uniqueness: true
+  validates :image, presence: true, uniqueness: true
 end
--- db/migrate/20240427093423_create_quiz_colors.rb
+++ db/migrate/20240427093423_create_quiz_colors.rb
@@ -1,18 +1,10 @@
 class CreateQuizColors < ActiveRecord::Migration[7.1]
   def change
     create_table :quiz_colors do |t|
-      t.string :name, null:false
-      t.integer :code_r, null:false
-      t.integer :code_g, null:false
-      t.integer :code_bclass CreateQuizImages < ActiveRecord::Migration[7.1]
-      def change
-        create_table :quiz_images do |t|
-          t.string :image
-    
-          t.timestamps
-        end
-      end
-    end
+      t.string :name, null: false
+      t.integer :code_r, null: false
+      t.integer :code_g, null: false
+      t.integer :code_b
 
       t.timestamps
     end
